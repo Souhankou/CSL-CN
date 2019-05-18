0468d746af9f7ed642e402d42b7ec52997053667
张秋宇 <aa940724@outlook.com>
2019-05-18 12:42:23 +0800

    chore:更换Angular+NetCore基本框架

--- Server/HanJie.CSLCN.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HanJie.CSLCN.Data
-{
-    public class Class1
-    {
-    }
-}
--- Server/HanJie.CSLCN.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HanJie.CSLCN.Service
-{
-    public class Class1
-    {
-    }
-}
